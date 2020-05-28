35494278ccb1f82c0e0f626eba13ca76f2726bfd
henrik schinke <schinkehenrik@gmail.com>
2020-05-28 11:01:58 +0200

    minor updates

--- Extract_data_from_pdf.R
+++ Extract_data_from_pdf.R
@@ -1,27 +1,27 @@
-## ----setup, include=FALSE------------------------------------------------
+## ----setup, include=FALSE--------------------------------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
 rm(list=ls())
 
 
-## ----load pdftools to extract values and tidyverse to process------------
+## ----load pdftools to extract values and tidyverse to process--------------------------------------
 library(pdftools)
 suppressMessages(library(tidyverse))
 
 
-## ----extract 96-well from pdf-file, echo=T-------------------------------
+## ----extract 96-well from pdf-file, echo=T---------------------------------------------------------
 file <- "template_data2.pdf"
 pdf_file <- file.path("~/Desktop/R/3_GitHub/Extract_data_from_pdf/",file)
 text <- suppressMessages(pdf_text(pdf_file))
 print(text)
 
 
-## ----process data with tidyverse-----------------------------------------
+## ----process data with tidyverse-------------------------------------------------------------------
 text <- strsplit(text, "\n")
 text_dat <- as.data.frame(text)
 print(text_dat)
 
 
-## ----rows containing data of interest, echo=T----------------------------
+## ----rows containing data of interest, echo=T------------------------------------------------------
 i = 1
 for(i in 1:nrow(text_dat)){
   if("A" == strsplit(as.character(text_dat[i,]), "\\s+")[[1]][1])
@@ -30,19 +30,19 @@ for(i in 1:nrow(text_dat)){
 print(as.character(text_dat[n:(n+7),]))
 
 
-## ----select row containing actual data-----------------------------------
+## ----select row containing actual data-------------------------------------------------------------
 text_values <- text_dat[n:(n+7),]
 text_values <- as.character(text_values)
 text_values <- as.data.frame(text_values)
 print(text_values)
 
 
-## ----process actual data-------------------------------------------------
+## ----process actual data---------------------------------------------------------------------------
 dat <- separate(text_values,text_values,into = c(as.character(c(0:12))), sep="\\s+")
 str(dat)
 
 
-## ----tidy data frame-----------------------------------------------------
+## ----tidy data frame-------------------------------------------------------------------------------
 tidy.dat <- dat[,2:13]
 tidy.dat <- sapply(tidy.dat, as.numeric)
 row.names(tidy.dat) <- dat[,1]
